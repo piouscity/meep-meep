1a60d87696c277ab06d79073e3036438049009e4
Anh-Hao Tran <JATC1024@gmail.com>
2018-04-03 22:07:11 +0700

    Hàm Year
    Hàm Year và file test

---
+++ Year.asm
@@ -0,0 +1,17 @@
+Year:
+	addi $v0, $zero, 0 # Gan ket qua bang 0.
+	addi $t0, $zero, 10 # So 10.
+	addi $t1, $a0, 6 # Vi tri cua chu so dau tien trong thang.
+	addi $t3, $a0, 10 # Vi tri ket thuc cua nam.
+	Year_iterates_in_year:
+		slt $t2, $t1, $t3 # Chua duyet xong nam.
+		beq $t2, $zero, end_iterates_in_year
+		mult $v0, $t0 # Nhan 10 ket qua.
+		mflo $v0 # Lay ket qua phep nhan.
+		lb $t2, 0($t1) # Lay chu so hien tai trong nam.
+		add $v0, $v0, $t2 # Gan no vo ket qua.
+		addi $v0, $v0, -48 # Doi tu char sang int.
+		addi $t1, $t1, 1 # Tang vi tri chu so trong nam.
+		j Year_iterates_in_year
+	end_iterates_in_year:
+	jr $ra
