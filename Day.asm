f2908c19c558e3bf2d987a30a1279d1625296b65
Anh-Hao Tran <JATC1024@gmail.com>
2018-04-03 21:27:39 +0700

    Hàm Day
    Day.asm: hàm Day.
    Day_test.asm: dùng để test hàm Day.

---
+++ Day.asm
@@ -0,0 +1,10 @@
+Day:
+	addi $t0, $zero, 10 # So 10.
+	lb $v0, 0($a0) # Lay chu so dau tien trong ngay.
+	addi $v0, $v0, -48 # Doi no thanh so.
+	mult $v0, $t0 # Nhan 10.
+	mflo $v0 # Lay ket qua phep nhan.
+	lb $t1, 1($a0) # Lay chu so thu hai trong ngay.
+	add $v0, $v0, $t1 # Cong vao ket qua.
+	addi $v0, $v0, -48
+	jr $ra
