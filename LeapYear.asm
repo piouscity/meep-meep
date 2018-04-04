cbc7fe1f7698ff34919ef50b50a40889ed66afcf
Anh-Hao Tran <JATC1024@gmail.com>
2018-04-04 11:23:48 +0700

    Update
    Đổi tên nhãn

--- LeapYear.asm
+++ LeapYear.asm
@@ -6,22 +6,22 @@ LeapYear:
 	addi $t0, $zero, 400 # So 400.
 	div $v0, $t0 # Kiem tra chia het cho 400.
 	mfhi $t0 # Lay phan du.
-	beq $t0, $zero, leap # Neu chia het cho 400 thi la nam nhuan.
+	beq $t0, $zero, leap_true # Neu chia het cho 400 thi la nam nhuan.
 
 	addi $t0, $zero, 100 # So 100.
 	div $v0, $t0 # Kiem tra chia het cho 100.
 	mfhi $t0
-	beq $t0, $zero, non_leap # Neu chia het cho 100 thi khong la nam nhuan.
+	beq $t0, $zero, leap_false # Neu chia het cho 100 thi khong la nam nhuan.
 
 	addi $t0, $zero, 4 # So 4.
 	div $v0, $t0 # Kiem tra chia het cho 4.
 	mfhi $t0
-	bne $t0, $zero, non_leap # Neu khong chia het cho 4 thi khong la nam nhuan.
+	bne $t0, $zero, leap_false # Neu khong chia het cho 4 thi khong la nam nhuan.
 
-	leap:
+	leap_true:
 		addi $v0, $zero, 1
 		j end_leap_year
-	non_leap:
+	leap_false:
 		addi $v0, $zero, 0
 end_leap_year:
 	lw $ra, 0($sp) # Lay lai gia tri ra.
