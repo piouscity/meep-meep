17937031840e78545aea182494c005a6203324ad
Nguyen Khac Duc <1612845@student.hcmus.edu.vn>
2018-04-05 15:43:10 +0700

    Update Option3.asm

--- Option3.asm
+++ Option3.asm
@@ -1,10 +1,4 @@
 Option3:
-	addi $a0, $s0, 0
-	addi $a1, $s1, 0
-	addi $a2, $s2, 0
-	addi $a3, $s3, 0 # Lay tham so la chuoi time
-	jal Date # Khoi tao chuoi time theo dung format.
-
 	addi $a0, $v0, 0 # Dat tham so la chuoi time da dinh dang.
 	jal Weekday
 
